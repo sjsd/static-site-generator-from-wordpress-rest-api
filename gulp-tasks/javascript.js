193f8c2659e377560238653122ec8499c1af2cc7
Helge Johnsen <hj@escio.no>
2018-02-02 08:20:57 +0100

    Fixing indendt in code

--- gulp-tasks/javascript.js
+++ gulp-tasks/javascript.js
@@ -1,11 +1,11 @@
 // Build JS
 module.exports = function (gulp, plugins) {
 	return function (done) {
-	gulp.src('./src/javascript/*.js')
-		.pipe(plugins.babel({
-			presets: ['env']
-		}))
-		.pipe(gulp.dest('./build/javascript'));
-	done();
+		gulp.src('./src/javascript/*.js')
+			.pipe(plugins.babel({
+				presets: ['env']
+			}))
+			.pipe(gulp.dest('./build/javascript'));
+		done();
 	};
 };
--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -2,10 +2,10 @@
 module.exports = function (gulp, plugins) {
 	return function (done) {
 		gulp.src('./src/sass/main.scss')
-			.pipe(plugins.sass({
-				outputStyle: 'compressed'
-			}))
-			.pipe(gulp.dest('./build/css'));
-			done();
+		.pipe(plugins.sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(gulp.dest('./build/css'));
+		done();
 	};
 };
